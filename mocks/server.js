dbdecef55842627e6b96fbc89333ba3bbcee7d1c
Yurii Hierasin <yurii.hierasin@visahq.ua>
2020-11-05 18:05:34 +0200

    Add documents endopints and mocks

--- mocks/server.js
+++ mocks/server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create()
 // const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = 3031
-
+const formidable = require('formidable')
 
 server.use(middlewares)
 // server.use(router)
@@ -63,3 +63,115 @@ server.get('/destinations', (req, res) => {
 server.get('/citizenships', (req, res) => {
     res.send(require('./data/citizenships'))
 })
+
+
+var orderFiles = require('./data/order-files');
+
+server.get('/orders/:id/files', (req, res) => {
+    res.send(orderFiles);
+});
+
+var orderItemFiles = require('./data/order-item-files');
+
+server.get('/orders/:id/services/:serviceId/files', (req, res) => {
+    res.send(orderItemFiles);
+});
+
+server.put('/orders/:id/files/:fileId', (req, res) => {
+    let success = false;
+
+    orderFiles
+        .forEach((orderFile, index) => {
+            if (orderFile.id === Number(req.params.fileId)) {
+                orderFiles[index] = req.body;
+                success = true;
+            }
+        });
+
+    if (success) {
+        res.send(req.body);
+    }
+
+    if (!success) {
+        res.status(400);
+        res.send('Bad request');
+    }
+});
+
+server.put('/orders/:id/services/:orderItemId/files/:fileId', (req, res) => {
+    let success = false;
+
+    orderItemFiles
+        .forEach((orderFile, index) => {
+            if (orderFile.id === Number(req.params.fileId)) {
+                orderItemFiles[index] = req.body;
+                success = true;
+            }
+        });
+
+    if (success) {
+        res.send(req.body);
+    }
+
+    if (!success) {
+        res.status(400);
+        res.send('Bad request');
+    }
+});
+
+server.delete('/orders/:id/services/:orderItemId/files/:fileId', (req, res) => {
+    let fileIndexes = orderItemFiles
+        .forEach((document, index) => {
+            if (document.id === Number(req.params.fileId)) {
+                orderItemFiles.splice(index, 1);
+                res.send('');
+                res.end();
+            }
+        });
+
+    res.status(404);
+    res.send('Not found');
+});
+
+server.post('/orders/:id/services/:orderItemId/files', (req, res) => {
+    let form = new formidable.IncomingForm();
+    let resultFile = null;
+    form.parse(req)
+        .on('file', function(name, file) {
+            resultFile = {
+                id: Math.floor(Math.random() * (1000 - 1) + 1),
+                author: 'Test author',
+                date: new Date(),
+                name: file.name,
+                tags: [],
+                url: 'http://localhost:3333' + file.path
+            };
+        })
+        .on('field', function(name, field) {
+            console.log('Got a field:', name);
+        })
+        .on('error', function(err) {
+            next(err);
+        })
+        .on('end', function() {
+            orderItemFiles.push(resultFile);
+            res.send(resultFile);
+        });
+});
+
+server.delete('/orders/:id/files/:fileId', (req, res) => {
+    let fileIndexes = orderFiles
+        .filter(document => document.id === Number(req.params.fileId))
+        .map((document, index) => { return index });
+
+    if (fileIndexes.length > 0) {
+        let index = fileIndexes[0];
+        orderFiles = orderFiles.splice(index, 1);
+        res.send('');
+    }
+
+    if (fileIndexes.length === 0) {
+        res.status(404);
+        res.send('Not found');
+    }
+});
